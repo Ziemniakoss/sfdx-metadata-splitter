15acfb9d219279461a4c3457012aad7ea5ecc56f
Przemek <rawa.przemek@gmail.com>
2022-11-07 22:06:34 +0100

    delete legacy commands

--- src/commands/metadata/labels/merge.ts
+++
@@ -1,8 +0,0 @@
-import MergeLabels from "../../splitter/labels/merge";
-
-export default class OldMergeLabels extends MergeLabels {
-	async run() {
-		this.ux.warn("This command was moved to splitter namespace");
-		return super.run();
-	}
-}
